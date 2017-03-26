aabfccf08684ff5852c0582cc9106f5e7d2eeff9
George Z. Zachos <gzachos@cse.uoi.gr>
2017-03-27 01:29:48 +0300

    Added pi_parallel.c and time_pi.sh

---
+++ hw1/pi_parallel.c
@@ -0,0 +1,128 @@
+/* pi.c
+ * ----
+ * Multithreaded pi calculation using POSIX threads
+ * and dynamic scheduling.
+ *
+ * Developed by George Z. Zachos
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+#include <sys/time.h>
+
+#define N 500000000   /* Intervals */
+
+
+typedef struct timeval timeval_t;
+
+
+/* Global declarations and initializations */
+double		W = 1.0 / ((double) N), W2, pi = 0.0;
+int		chunksize, /* K */
+		taskid,    /* the next task id to execute */
+		ntasks;    /* total number of tasks */
+pthread_mutex_t	mut = PTHREAD_MUTEX_INITIALIZER;
+
+
+/* Function prototypes */
+void taskexecute(int taskid);
+void *thrfunc(void *arg);
+
+
+int main(int argc, char **argv)
+{
+	int        i,
+	           nthr;   /* number of threads */
+	pthread_t *tid;
+	timeval_t  tv1, tv2;
+	double     elapsed_time;
+
+	/* Number of threads and chunk size are provided
+	 * as command line arguments.
+	 */
+	if (argc < 3)
+	{
+		fprintf(stderr, "Invalid number of command-line arguments!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	nthr      = atoi(argv[1]);
+	chunksize = atoi(argv[2]);
+
+	if (nthr < 1)
+	{
+		fprintf(stderr, "Invalid number of threads!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (chunksize < 1)
+	{
+		fprintf(stderr, "Chunk size cannot be less than 1!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (N % chunksize != 0)
+	{
+		fprintf(stderr, "Chunk size must evenly divide the number of intervals!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	ntasks = N/chunksize;
+	W2     = W*W;   /* Save some calculations */
+	W      = 4*W;
+
+	tid = (pthread_t *) malloc(nthr * sizeof(pthread_t));
+	if (!tid)
+	{
+		perror("malloc");
+		exit(errno);
+	}
+
+	/* Start timing */
+	gettimeofday(&tv1, NULL);
+	for (i = 0; i < nthr; i++)
+		pthread_create(tid+i, NULL, thrfunc, NULL);
+	for (i = 0; i < nthr; i++)
+		pthread_join(tid[i], NULL);
+	/* End timing */
+	gettimeofday(&tv2, NULL);
+
+	elapsed_time =	(tv2.tv_sec - tv1.tv_sec) +
+			(tv2.tv_usec - tv1.tv_usec)*1.0E-6;
+
+	printf("nthr = %d\tchunk = %d\tpi = %.10lf\ttime: %lf sec.\n", nthr, chunksize, pi, elapsed_time);
+	return 0;
+}
+
+
+void taskexecute(int taskid)
+{
+	int    i;
+	double tmp_sum = 0.0;
+
+	for (i = taskid*chunksize; i < (taskid+1)*chunksize; i++)
+		tmp_sum += W / (1.0 + (0.5 + (double) i)*(0.5 + (double) i)*W2);
+
+	pthread_mutex_lock(&mut);
+	pi += tmp_sum;
+	pthread_mutex_unlock(&mut);
+}
+
+
+void *thrfunc(void *arg)
+{
+	int t;
+
+	while (1)
+	{
+		pthread_mutex_lock(&mut);
+		t = taskid++;
+		pthread_mutex_unlock(&mut);
+		if (t >= ntasks)
+			break;
+		taskexecute(t);
+	}
+	return NULL;
+}
---
+++ hw1/time_pi.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+NUM_THREADS="1 4 16"
+CHUNK_SIZE="1 10 100 1000 10000 100000"
+
+gcc -pthread pi_parallel.c --output pi
+
+for nthr in ${NUM_THREADS}
+do
+	for chnk in ${CHUNK_SIZE}
+	do
+		x=0
+		while [ $x -lt 4 ]
+		do
+			./pi ${nthr} ${chnk}
+			((x += 1))
+		done
+		echο -e "\n"
+	done
+done
