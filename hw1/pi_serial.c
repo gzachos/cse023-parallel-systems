817192f1adaf9b8d3cf3a47a7912767e441c2ed8
George Z. Zachos <gzachos@cse.uoi.gr>
2017-03-26 22:31:43 +0300

    Styling and main definition changed in pi_serial.c

--- hw1/pi_serial.c
+++ hw1/pi_serial.c
@@ -1,24 +1,25 @@
-/* pi.c
- * ----
- *   Serial pi calculation.
+/* pi_serial.c
+ * -----------
+ * Serial pi calculation.
  *
  * VVD
  */
 #include <stdio.h>
 
-#define N     10000000      /* Intervals */
+#define N 10000000   /* Intervals */
 
 
-main()
+int main(void)
 {
-  int    i;
-  double W = 1.0 / ((double) N), W2, pi = 0.0;
+	int    i;
+	double W = 1.0 / ((double) N), W2, pi = 0.0;
 
-  W2 = W*W;   /* Save some calculations */
-  W = 4*W;
-  for (i = 0; i < N; i ++)
-    pi += W / (1.0 + (0.5 + (double) i)*(0.5 + (double) i)*W2);
-  printf("pi = %.10lf\n", pi);
+	W2 = W*W;   /* Save some calculations */
+	W = 4*W;
+	for (i = 0; i < N; i ++)
+		pi += W / (1.0 + (0.5 + (double) i)*(0.5 + (double) i)*W2);
+	printf("pi = %.10lf\n", pi);
+	return 0;
 }
 
 
