01a27828681bf78033985fd43a34aa1e2f0a3498
George Z. Zachos <gzachos@cse.uoi.gr>
2017-06-12 21:34:31 +0300

    Added plots and corresponding files in hw3/

---
+++ hw3/ex3/pi.plt
@@ -0,0 +1,18 @@
+#!/usr/bin/env gnuplot --persist
+
+set terminal pngcairo enhanced font "arial,10" fontscale 1.0 size 400, 300
+set output './plots/matmul.png'
+set bar 1.000000 front
+set style line 1 lc rgb '#dd181f' lt 1 lw 1 pt 5 pi -1 ps 1
+set style fill transparent solid 0.5 noborder
+set title "Pi calculation"
+set xlabel "MPI Nodes\n(x4 MPI processes)"
+set ylabel "Time (sec)"
+set logscale y 2
+set grid
+set yrange [:32]
+set colorbox vertical origin screen 0.9, 0.2, 0 size screen 0.05, 0.6, 0 front bdefault
+plot './data/final.dat' using 1:($3+$6) title 'Comm.' with filledcurves x1 lc rgb "#FF7F50", \
+     './data/final.dat' using 1:3 title 'Calc.' with filledcurves x1 lc rgb "#0000CD", \
+     './data/final.dat' using 1:2 title 'Total' with linespoints ls 1
+
---
+++ hw3/ex3/plot.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+OUTFILE="./data/final.dat"
+
+function calc() {
+	SUM=`paste  ./data/pi_n${1}* | grep "${2}" | awk '{print $3" "$6" "$9" "$12}' | tr " " "+" | bc -l`
+	RES=`echo ${SUM}/4 | bc -l | sed '/\./ s/\.\{0,1\}0\{1,\}$//'`
+}
+
+function stats() {
+	OUT="${1} "
+	calc ${1} 'Total' 
+	OUT="$OUT $RES"
+	calc ${1} 'Avg Calc'
+	OUT="$OUT $RES"
+	calc ${1} 'Min Calc'
+	OUT="$OUT $RES"
+	calc ${1} 'Max Calc'
+	OUT="$OUT $RES"
+	calc ${1} 'Avg Comm'
+	OUT="$OUT $RES"
+	calc ${1} 'Min Comm'
+	OUT="$OUT $RES"
+	calc ${1} 'Max Comm'
+	OUT="$OUT $RES"
+}
+
+if [ ! -d "./plots/" ]
+then
+        mkdir plots
+fi
+
+stats 2 
+echo `echo $OUT | tr "\n" " "` > ${OUTFILE}
+stats 4
+echo `echo $OUT | tr "\n" " "` >> ${OUTFILE}
+stats 8
+echo `echo $OUT | tr "\n" " "` >> ${OUTFILE}
+
+gnuplot pi.plt
+
